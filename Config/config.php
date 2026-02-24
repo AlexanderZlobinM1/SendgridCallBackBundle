fe9209f817378978840dc79edbcf6413d0ec68bd
AlexanderZlobinM1 <alexander.zlobin@m1.rs>
2026-02-24 15:22:23 +0100

    Update author name to Alexander Zlobin

--- Config/config.php
+++ Config/config.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     'name'        => 'SendGrid Callback',
     'description' => 'Handles SendGrid Event Webhook callbacks for bounce and complaint events.',
-    'author'      => 'Alexey Zlobin',
+    'author'      => 'Alexander Zlobin',
     'version'     => '1.0.0',
 ];
